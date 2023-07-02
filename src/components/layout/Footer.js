733a9d52c4c515c998e3aff1e8820b3c3508c0d6
Alexandro Silva <92386412+AlexandroSilvaDev@users.noreply.github.com>
2023-07-02 17:20:09 -0300

    Criando a Home

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 import styles from './Footer.module.css'
-import { Link } from "react-router-dom";
 
 function Footer()
 {
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton';
 
 function Home()
 {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerencias os seus projetos agora mesmo!</p>
+            <LinkButton to="/NewProject" text="Criar Projeto" />
+            <img src={savings} alt="Costs" />
+        </section>
+    )
 }
 
 export default Home

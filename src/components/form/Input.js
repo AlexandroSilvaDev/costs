ac40e83e704f3433e02cca55b49281113ca1e6ea
Alexandro Silva <92386412+AlexandroSilvaDev@users.noreply.github.com>
2023-07-03 22:06:00 -0300

    Correção sobre o problema nos inputs dos projetos

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -5,9 +5,10 @@ function Input({type, text, name, placeholder, handleOnChange, value})
 {
     return (
         <div className={styles.form_control}>
-            <label htmlFor={name}>{text}</label>
+            <label htmlFor={name}>{text}:</label>
             <input 
-                type={type} 
+                type={type}
+                name={name} 
                 id={name} 
                 placeholder={placeholder} 
                 onChange={handleOnChange} 
